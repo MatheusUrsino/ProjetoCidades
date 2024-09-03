e63c7cd1ac93926da89b49c6201ca2160fecf2b1
Matheus Ursino dos Santos <66560955+MatheusUrsino@users.noreply.github.com>
2024-09-02 22:09:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+// script feito para o funcionamento do carrossel de imagens
+const swiper = new Swiper('.slider-wrapper', {
+
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 20,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints:
+    {
+        0: {
+            slidesPerView: 1
+        },
+        620: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+
+  });
+  
